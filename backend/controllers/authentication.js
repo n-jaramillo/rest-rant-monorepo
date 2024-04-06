fa860bf906c1296c62483c145d443b01c8e58c7b
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2024-04-05 19:09:17 -0700

    part 3: setting up sessions in rest-rant using cookie-session and credentials

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -5,19 +5,32 @@ const bcrypt = require('bcrypt')
 const { User } = db
 
 router.post('/', async (req, res) => {
-    
+
     let user = await User.findOne({
         where: { email: req.body.email }
     })
 
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
-        res.status(404).json({ 
-            message: `Could not find a user with the provided username and password` 
+        res.status(404).json({
+            message: `Could not find a user with the provided username and password`
         })
     } else {
+        res.session.userId = user.userId
         res.json({ user })
     }
 })
 
+router.get('/profile', async (req, res) => {
+    try {
+        let user = await User.findOne({
+            where: {
+                userId: req.session.userId
+            }
+        })
+        res.json(user)
+    } catch {
+        res.json(null)
+    }
+})
 
 module.exports = router
