0dd2f7b1a9e6197c7834e4c27f7f15477544c8b9
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2024-04-05 19:33:46 -0700

    part 4.3 added jwt token handling in authentication controller

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -1,20 +1,22 @@
 const router = require('express').Router()
 const db = require("../models")
 const bcrypt = require('bcrypt')
+const jwt = require('jwt')
 
 const { User } = db
 
 router.post('/', async (req, res) => {
-    
+
     let user = await User.findOne({
         where: { email: req.body.email }
     })
 
     if (!user || !await bcrypt.compare(req.body.password, user.passwordDigest)) {
-        res.status(404).json({ 
-            message: `Could not find a user with the provided username and password` 
+        res.status(404).json({
+            message: `Could not find a user with the provided username and password`
         })
     } else {
+        const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
         res.json({ user })
     }
 })
