629563df1f02b3f6b718c81e41d5da2b6c615644
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2024-04-05 20:02:50 -0700

    updated authentication controller and CurrentUser context

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -17,25 +17,19 @@ router.post('/', async (req, res) => {
         })
     } else {
         const result = await jwt.encode(process.env.JWT_SECRET, { id: user.userId })
-        res.json({ user, token: result.value })
+        res.json({ user: user, token: result.value })
     }
 })
 
 router.get('/profile', async (req, res) => {
     try {
-        // Split the authorization header into [ "Bearer", "TOKEN" ]:
-        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+        const [PaymentMethodChangeEvent, token] = req.headers.authorization.split(' ')
 
-        //Only handler "Bearer" authorization for now
-        if (authenticationMethod == 'Bearer') {
-            // Decode the JWT
+        if (PaymentMethodChangeEvent == 'Bearer') {
             const result = await jwt.decode(process.env.JWT_SECRET, token)
-
-            // Get the logged in user's id from the payload
             const { id } = result.value
         }
 
-        // Find the user object using their id:
         let user = await User.findOne({
             where: {
                 userId: id
--- frontend/src/contexts/CurrentUser.js
+++ frontend/src/contexts/CurrentUser.js
@@ -18,7 +18,6 @@ function CurrentUserProvider({ children }) {
         }
         getLoggedInUser()
     }, [])
-    window.setCurrentUser = setCurrentUser
     return (
         <CurrentUser.Provider value={{ currentUser, setCurrentUser }}>
             {children}
