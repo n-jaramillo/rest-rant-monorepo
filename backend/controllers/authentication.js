fb98e0436fc41ea70c30224b04a772ce41b42948
n-jaramillo <102261871+n-jaramillo@users.noreply.github.com>
2024-04-05 19:43:48 -0700

    part 4.6: updated authentication controller to handle jwt and user auth

--- backend/controllers/authentication.js
+++ backend/controllers/authentication.js
@@ -23,12 +23,25 @@ router.post('/', async (req, res) => {
 
 router.get('/profile', async (req, res) => {
     try {
-        // let user = await User.findOne({
-        //     where: {
-        //         userId:
-        //     }
-        // })
-        // res.json(user)
+        // Split the authorization header into [ "Bearer", "TOKEN" ]:
+        const [authenticationMethod, token] = req.headers.authorization.split(' ')
+
+        //Only handler "Bearer" authorization for now
+        if (authenticationMethod == 'Bearer') {
+            // Decode the JWT
+            const result = await jwt.decode(process.env.JWT_SECRET, token)
+
+            // Get the logged in user's id from the payload
+            const { id } = result.value
+        }
+
+        // Find the user object using their id:
+        let user = await User.findOne({
+            where: {
+                userId: id
+            }
+        })
+        res.json(user)
     } catch {
         res.json(null)
     }
